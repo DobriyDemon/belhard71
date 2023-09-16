0a08bbfdf7b7ebb049b01b93b0492cb12be0aaf1
Arseniy <aarden39@gmail.com>
2023-09-16 08:51:10 +0300

    Homework Lesson_3_1 complete

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 
+user_input = input("Enter something: ")
+result_1_1 = user_input.replace(" ", "-")
+print(result_1_1)
 
+
+splitted = user_input.split()
+print(splitted)
+result_2_1 = "-".join(splitted)
+print(result_2_1)
