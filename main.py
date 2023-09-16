40b76b71a260149d01dc13e2e8f5fd5b566dcf92
Arseniy <aarden39@gmail.com>
2023-09-16 11:12:50 +0300

    Lesson_3_4 complete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #Created Homework branch for the main file
-num1 = input("Enter first number: ")
-num2 = input("Enter second number: ")
-num3 = input("Enter third number: ")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+print(f"Result: {(num1 > 0)+(num2 > 0)+(num3 > 0)} number(s) are positive")
