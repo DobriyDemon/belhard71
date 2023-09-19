12a2b264a827e9ea91ced70d4ffcbe4d9f618bb9
Arseniy <aarden39@gmail.com>
2023-09-19 21:27:46 +0300

    Lesson_4_2 process

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ data_email = defaultdict(list)
 data_2 = defaultdict(list)
 count_of_users = input('Enter count of users: ')
 #data_1[input('Enter username: ')].append(input('Enter email: '))
-data_2 = {i: data_username[input('Enter username: '), input('Enter email: ')] for j in count_of_users}
-
+data_2 = {i: data_username[i].append(input("Enter username: ")), i: data_email[i].append(input("Enter email: ")) for i in range(int(count_of_users))}
+print(data_username)
 print(data_2)
