6cce83218b8ccede227fb7c155aa9668f8e6da6b
Arseniy <aarden39@gmail.com>
2023-09-19 18:29:02 +0300

    Lesson_3_4 homework in process

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 
 list_of_numbers = []
 input_n = 0
-count_of_numbers = input("Enter the max degree of the number: ")
-for i in range(input_n):
-    list_of_numbers.append(int())
+number = int(input("Enter the number: "))
+degree = int(input("Enter the max degree of the number: "))
+for i in range(degree):
+    list_of_numbers.append(int(number ** i))
+
+print(f"List of numbers: {list_of_numbers}")
+print(f"Max degree is: {degree}")
