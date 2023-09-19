9c1726b6177f6abbd87d8c37cb9494635c3379fa
Arseniy <aarden39@gmail.com>
2023-09-19 18:21:07 +0300

    Lesson_3_4 started homework

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 #Created Homework branch for the main file
 
-
+list_of_numbers = []
+input_n = 0
+count_of_numbers = input(f"Enter the max degree of the number: {input_n}")
+for i in range(input_n):
+    list_of_numbers.append(int())
