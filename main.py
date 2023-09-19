b9b197a8270f6f12d2e72f0907d4a9092285bf33
Arseniy <aarden39@gmail.com>
2023-09-19 21:16:58 +0300

    Lesson_4_2 process

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-#Created Homework branch for the main file
+# Created Homework branch for the main file
+from collections import defaultdict
+
+data_username = defaultdict(list)
+data_email = defaultdict(list)
+data_2 = defaultdict(list)
+count_of_users = input('Enter count of users: ')
+#data_1[input('Enter username: ')].append(input('Enter email: '))
+data_2 = {i: data_username[input('Enter username: '), input('Enter email: ')] for j in count_of_users}
+
+print(data_2)
