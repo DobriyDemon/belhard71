db0b2a8524a66abf0aa33b3c0c637c33389669a2
Arseniy <aarden39@gmail.com>
2023-09-19 20:52:58 +0300

    Lesson_4_1 complete

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 #Created Homework branch for the main file
 
 list_of_numbers = []
-input_n = 0
 number = int(input("Enter the number: "))
 degree = int(input("Enter the max degree of the number: "))
 for i in range(degree):
-    list_of_numbers.append(int(number ** i))
+    list_of_numbers.append(int(number ** (i+1)))
 
 print(f"List of numbers: {list_of_numbers}")
 print(f"Max degree is: {degree}")
