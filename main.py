1c9e6527524b89b9abc227c2cf55f7967f22da38
Arseniy <aarden39@gmail.com>
2023-09-20 22:11:07 +0300

    Lesson_4_3 complete successfully, but i not sure that's right solve

--- main.py
+++ main.py
@@ -6,5 +6,4 @@ data_username_and_email = {}
 data_output = defaultdict(list)
 data_username_and_email['username_and_email'] = [input("Enter username: ") + ", " + input("Enter email: ") for i in range(count_of_users)]
 data_output = {i: data_username_and_email['username_and_email'][i] for i in range(count_of_users)}
-print(f"Data username and email: {data_username_and_email}")
 print(f"Data output: {data_output}")
