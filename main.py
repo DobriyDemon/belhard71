74ca21cefb57ac03d2bcc7e6b434a28a4f974d57
Arseniy <aarden39@gmail.com>
2023-09-20 21:34:21 +0300

    Lesson_4_3 not right success in this task of homework

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # Created Homework branch for the main file
 from collections import defaultdict
-count_of_users = int(input("Enter count of users: "))
-data_username_and_email = []
-data_username_and_email = {'username': [input("Enter username: ")], 'email': [input("Enter email: ")]}
-data_output = defaultdict(list)
-data_output.fromkeys(['name'],[data_username_and_email])
 
-print(f"Data username: {data_username_and_email}")
+count_of_users = int(input("Enter count of users: "))
+data_username_and_email = {}
+data_output = defaultdict(list)
+data_username_and_email['username_and_email'] = [input("Enter username: ") + ", " + input("Enter email: ") for i in range(count_of_users)]
+data_output = {i:data_username_and_email['username_and_email'][i] for i in range(count_of_users)}
+print(f"Data username and email: {data_username_and_email}")
 print(f"Data output: {data_output}")
 # print(f"List: {list_username_and_email_values}")
 # print(f"Data 1: {data_1}")
