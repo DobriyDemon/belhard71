10bfb5cedc77c58bc025567f7bff056cd1320392
Arseniy <aarden39@gmail.com>
2023-09-20 20:08:57 +0300

    Lesson_4_3 process

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # Created Homework branch for the main file
 from collections import defaultdict
+count_of_users = int(input("Enter count of users: "))
+data_username_and_email = []
+data_username_and_email = {'username': [input("Enter username: ")], 'email': [input("Enter email: ")]}
+data_output = defaultdict(list)
+data_output.fromkeys(['name'],[data_username_and_email])
 
-data_username = defaultdict(list)
-data_email = defaultdict(list)
-data_2 = defaultdict(list)
-count_of_users = input('Enter count of users: ')
-#data_1[input('Enter username: ')].append(input('Enter email: '))
-data_2 = {i: data_username[i].append(input("Enter username: ")), i: data_email[i].append(input("Enter email: ")) for i in range(int(count_of_users))}
-print(data_username)
-print(data_2)
+print(f"Data username: {data_username_and_email}")
+print(f"Data output: {data_output}")
+# print(f"List: {list_username_and_email_values}")
+# print(f"Data 1: {data_1}")
+# print(f"Data 2: {data_2}")
