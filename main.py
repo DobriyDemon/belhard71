fc0036295a530cdf759ac5945fe739d09378029d
Arseniy <aarden39@gmail.com>
2023-09-21 18:41:49 +0300

    Lesson_5 Base info about if operator

---
+++ main.py
@@ -0,0 +1,11 @@
+# Operatory vetvlenia
+a = int(input("Enter number: "))
+
+if a > 0:# if a return True
+    print("Variable 'a' is positive")
+elif a < 0: # if a return False
+    print("Variable 'a' is negative")
+else: # if a return False in each case
+    print("Variable 'a' is zero")
+
+
