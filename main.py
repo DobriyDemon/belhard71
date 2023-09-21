99d92dcd043ea11d68107f3434d40147e843d0e0
Arseniy <aarden39@gmail.com>
2023-09-21 21:17:15 +0300

    Lesson_5_1 homework start

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 # Created Homework branch for the main file
 
+cout_of_output_nums = int(input("Enter the count of output numbers: "))
+more_than_K = int(input("Enter the number K: "))
+num_M = int(input("Enter the number M: "))
+list_of_output_nums = []
+list_on_fife = []
+
+for i in range(cout_of_output_nums):
+    if i > more_than_K:
+        print("i is more than K: ")
+        if i % num_M == 0:
+            print("i is a multiple of M: ")
+            if len(list_on_fife) <= 5:
+                list_on_fife.append(i)
+                print("list_on_fife <=5: ", list_on_fife)
+            else:
+                list_on_fife.clear()
+                list_on_fife.append(i)
+
+                print("list_on_fife if list <=5(else): ", list_on_fife)
+            list_of_output_nums.append(list_on_fife)
+            print("list_of_output_nums: ", list_of_output_nums)
+print(list_of_output_nums)
