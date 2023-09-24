5c19f03719a485a2d1154567b0c5a07e393852d4
Arseniy <aarden39@gmail.com>
2023-09-24 23:20:40 +0300

    Lesson_5_2 started and has some progress

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 # Created Homework branch for the main file
-f
+print("Calculator!")
+num1 = int(input("Enter first number: "))
+action = input("Enter action: ")
+num2 = int(input("Enter second number: "))
+
+if action == "+":
+    result = num1 + num2
+elif action == "-":
+    result = num1 - num2
+elif action == "*":
+    result = num1 * num2
+elif action == "/":
+    result = num1 / num2
+elif action == "**":
+    result = num1 ** num2
+elif action == "%":
+    result = num1 % num2
+
+print("Result: ",num1, action, num2, "=", result)
