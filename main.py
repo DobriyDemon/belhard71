62f60a09c7ec18d6a8b4c56a5383506470cc6e65
Arseniy <aarden39@gmail.com>
2023-09-25 19:51:45 +0300

    Lesson_5_3 completed

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 # Created Homework branch for the main file
+# Вывести четные числа от 2 до N по 5 в строку
+
+list_of_fife = []
+
+for i in range(2, int(input("Enter end of range: ")) + 1, 2):
+    list_of_fife.append(i)
+    if len(list_of_fife) >= 5:
+        print(list_of_fife)
+        list_of_fife.clear()
 
