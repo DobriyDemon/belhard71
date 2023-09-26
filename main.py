8f1be00384899b4dbeab879c48accec317d476a5
Arseniy <aarden39@gmail.com>
2023-09-26 20:47:00 +0300

    Lesson_6_2 in work

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-# Created Homework branch for the main file
+# Task 2
+text = input("Enter text: ")
+test_result = None
+text_splitted = text.split()
+dict_text = {}
+dict_morse = {}
+for i in text:
+    dict_text
 
+print(dict_text)
+print(text_splitted)
