73233c75fb100136c30bd91ac3bed09852e4fe6b
Arseniy <aarden39@gmail.com>
2023-09-26 20:50:59 +0300

    Lesson_6_2 in work

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 # Task 2
 text = input("Enter text: ")
-test_result = None
-text_splitted = text.split()
-dict_text = {}
-dict_morse = {}
-for i in text:
-    dict_text
 
-print(dict_text)
-print(text_splitted)
+def translate_to_morse(text: str):
+    pass
