d2839777fdc6713f0008d4185fb91caf6a8f7d30
Arseniy <aarden39@gmail.com>
2023-09-26 18:19:56 +0300

    Lesson_5_3 completed

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ list_on_fife = []
 for i in range(round(count_of_output_numbers/5)):
     for j in range(count_of_output_numbers * count_of_output_numbers):
         if j > more_than_K and j % num_M == 0:
-            print("Each if worked, j: ", j)
+            #print("Each if worked, j: ", j)
             list_on_fife.append(j)
         if len(list_on_fife) > 4:
             list_of_output_nums.append(list_on_fife.copy())
             list_on_fife.clear()
-        print(list_on_fife)
+        #print(list_on_fife)
         if len(list_of_output_nums) >= round(count_of_output_numbers/5):
             break
 
