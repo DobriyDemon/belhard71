b79ed39286aed80e8f7e6a92731175cc28c1e219
Arseniy <aarden39@gmail.com>
2023-09-27 17:51:39 +0300

    Lesson_6_2 completed

--- main.py
+++ main.py
@@ -1,34 +1,177 @@
 # Task 2
-MORSE_CODE_DICT = \
-    {  # Russian letters
-        'А': '.-', 'Б': '-...', 'В': '.--', 'Г': '--.', 'Д': '-..', 'Е': '.', 'Ё': '.',
-        'Ж': '...-', 'З': '--..', 'И': '..', 'Й': '.---', 'К': '-.-', 'Л': '.-..', 'М': '--',
-        'Н': '-.', 'О': '---', 'П': '.--.', 'Р': '.-.', 'С': '...', 'Т': '-', 'У': '..-',
-        'Ф': '..-.', 'Х': '....', 'Ц': '-.-.', 'Ч': '---.', 'Ш': '----', 'Щ': '--.-', 'Ъ': '--.--',
-        'Ы': '-.--', 'Ь': '-..-', 'Э': '..-..', 'Ю': '..--', 'Я': '.-.-',
-        # English letters
-        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.',
-        'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.',
-        'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-',
-        'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
-        # Numbers
-        '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
-        '6': '-....', '7': '--...', '8': '---..', '9': '----.',
-        # Special symbols
-        ',': '--..--', '.': '.-.-.-', '?': '..--..', '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-', ' ': ' '}
 
-user_text_input = input("Enter message: ")
-user_text_input_splitted = user_text_input.split()
-user_text_encrypted = []
-user_text_decrypted = []
-if user_text_input.isalpha():
-    # Encryptor
-    for letter in user_text_input.upper():
-        user_text_encrypted.append(MORSE_CODE_DICT[letter])
-    print("Encrypted message: ", " ".join(user_text_encrypted))
-else:
-    # Decryptor
-    for letter in user_text_input:
-        result_letter = list(zip())
-        user_text_decrypted.append(result_letter)
-    print("Decrypted message: ", user_text_decrypted)
+MORSE_CODE_DICT_RUS = {  # Russian letters
+    "А": ".-",
+    "Б": "-...",
+    "В": ".--",
+    "Г": "--.",
+    "Д": "-..",
+    "Е": ".",
+    # "Ё": ".",
+    "Ж": "...-",
+    "З": "--..",
+    "И": "..",
+    "Й": ".---",
+    "К": "-.-",
+    "Л": ".-..",
+    "М": "--",
+    "Н": "-.",
+    "О": "---",
+    "П": ".--.",
+    "Р": ".-.",
+    "С": "...",
+    "Т": "-",
+    "У": "..-",
+    "Ф": "..-.",
+    "Х": "....",
+    "Ц": "-.-.",
+    "Ч": "---.",
+    "Ш": "----",
+    "Щ": "--.-",
+    "Ъ": "--.--",
+    "Ы": "-.--",
+    "Ь": "-..-",
+    "Э": "..-..",
+    "Ю": "..--",
+    "Я": ".-.-",
+    # Numbers
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    # Special symbols
+    ",": "--..--",
+    ".": ".-.-.-",
+    "?": "..--..",
+    "/": "-..-.",
+    "-": "-....-",
+    "(": "-.--.",
+    ")": "-.--.-",
+    "!": "−−..−−",
+    " ": "/",
+    "": "",
+}
+MORSE_CODE_DICT_ENG = {  # English letters
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    # Numbers
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    # Special symbols
+    ",": "--..--",
+    ".": ".-.-.-",
+    "?": "..--..",
+    "/": "-..-.",
+    "-": "-....-",
+    "(": "-.--.",
+    ")": "-.--.-",
+    "!": "−−..−−",
+    " ": "/",
+    "": "",
+}
+
+
+MORSE_CODE_REVERSED_ENG = {value: key for key, value in MORSE_CODE_DICT_ENG.items()}
+MORSE_CODE_REVERSED_RUS = {value: key for key, value in MORSE_CODE_DICT_RUS.items()}
+
+language = input("Enter language(RUS/ENG): ")
+language = language.upper()
+text = input("Enter message: ")
+text = text.upper()
+action = input("Enter action(encode/decode): ")
+action = action.upper()
+
+
+def translate(
+    text: str,
+    timed_string="",
+) -> str:
+    global translated
+    if action == "ENCODE":
+        # Encode methods
+        if language == "RUS":
+            translated = ""
+            for letter in text:
+                if letter != " ":
+                    translated += MORSE_CODE_DICT_RUS[letter] + " "
+                else:
+                    translated += "/ "
+            print(translated)
+        elif language == "ENG":
+            translated = ""
+            for letter in text.upper():
+                if letter != " ":
+                    translated += MORSE_CODE_DICT_ENG[letter] + " "
+                else:
+                    translated += "/ "
+            print(translated)
+    elif action == "DECODE":
+        # Decode methods
+        if language == "RUS":
+            translated = ""
+            for symbol in text:
+                if symbol != " ":
+                    timed_string += symbol
+                else:
+                    if symbol == "/":
+                        timed_string += " "
+                    translated += MORSE_CODE_REVERSED_RUS[timed_string]
+                    timed_string = ""
+            if timed_string != "":
+                translated += MORSE_CODE_REVERSED_RUS[timed_string]
+            print(translated)
+        elif language == "ENG":
+            translated = ""
+            for symbol in text:
+                if symbol != " ":
+                    timed_string += symbol
+                else:
+                    if symbol == "/":
+                        timed_string += " "
+                    translated += MORSE_CODE_REVERSED_ENG[timed_string]
+                    timed_string = ""
+            if timed_string != "":
+                translated += MORSE_CODE_REVERSED_ENG[timed_string]
+            print(translated)
+            return translated
+
+
+translate(text)
