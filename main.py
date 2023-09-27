711f86ff5d0aeb3800775f2cb31903d943e45eda
Arseniy <aarden39@gmail.com>
2023-09-27 20:31:06 +0300

    Lesson_6_1 complete

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 # Created Homework branch for the main file
 # Task 4
-list_of_data = ['English', 12, 'Russian', 56, True, 'Italian', False]
-timed_data = {}
-user_input = input("Enter type of data: ")
+list_of_data = ['English', 12, 'Russian', (56, 54, 52, 50), {'name': 'John'}, 'Italian', False]
 
 
-def sorter(list_of_data, user_input):
-    print("The unsorted list is: ", list_of_data)
-    for i in list_of_data:
-        if i == str:
-            timed_data.
+def sort_strings_only(list_of_data):
+    return sorted(filter(lambda x: type(x) == str, list_of_data))
 
 
-sorter(list_of_data, user_input)
+print(sort_strings_only(list_of_data))
+
+sort_strings_only(list_of_data)
