bef5f80835c0c9cb568f57c447d4e54d3c80d406
Arseniy <aarden39@gmail.com>
2023-09-27 20:11:25 +0300

    Lesson_6_1 in work

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-#Task 1
+# Task 1
+string = input('Enter a decimal number: ')
 
-num = input("Enter a number: ")
-res = bin(int(num))
-print(res)
 
+def str_to_int(string):
+    result = 0
+    for char in string:
+        result = result * 10 + (ord(char) - ord('0'))
+    print(result)
+    print(type(result))
+    return result
+
+
+def decimal_to_binary(decimal):
+    binary = ''
+    while decimal > 0:
+        binary = str(decimal % 2) + binary
+        decimal //= 2
+    print(binary)
+    print(type(binary))
+    return binary
+
+
+decimal_to_binary(str_to_int(string))
