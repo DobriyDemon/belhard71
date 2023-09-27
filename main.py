1bd997a5c616c56a66648c385a4096cdd90afa7e
Arseniy <aarden39@gmail.com>
2023-09-27 20:37:45 +0300

    Lesson_6_5 complete

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # Created Homework branch for the main file
+# Task 5
+list_of_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(list_of_nums)
 
+
+def reverse(list_of_nums):
+    list_of_nums.sort(reverse = True)
+    print(list_of_nums)
+    return list_of_nums
+
+
+reverse(list_of_nums = list_of_nums)
