c005e7a9224f004dea11a2aa0f1e98d267319755
Arseniy <aarden39@gmail.com>
2023-09-27 21:37:38 +0300

    Lesson_6_7 completed

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ list_of_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print(list_of_nums)
 print("The length of the list is: ", len(list_of_nums))
 
-for i in range(len(list_of_nums) + 1):
-    if i != len(list_of_nums):
+for i in range(len(list_of_nums)):
+    if i != len(list_of_nums) - 1:
         sum_neighbors = list_of_nums[i - 1] + list_of_nums[i + 1]
     else:
-        sum_neighbors = list_of_nums[i - 1] + list_of_nums[i::-1]
-
+        sum_neighbors = list_of_nums[i - 1] + list_of_nums[0]
     print("The sum of the neibors of ", list_of_nums[i], " is: ", sum_neighbors)
