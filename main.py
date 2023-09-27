5ddf506872ea2d05bc4a4909d58044ec8145c31f
Arseniy <aarden39@gmail.com>
2023-09-27 19:15:42 +0300

    Lesson_6_3 in work

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 # Created Homework branch for the main file
+# Task 4
+list_of_data = ['English', 12, 'Russian', 56, True, 'Italian', False]
+timed_data = {}
+user_input = input("Enter type of data: ")
 
+
+def sorter(list_of_data, user_input):
+    print("The unsorted list is: ", list_of_data)
+    for i in list_of_data:
+        if i == str:
+            timed_data.
+
+
+sorter(list_of_data, user_input)
