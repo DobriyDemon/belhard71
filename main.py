b63d0e5fc096e9a4d4415d4484dad394ec17040c
Arseniy <aarden39@gmail.com>
2023-09-27 20:17:52 +0300

    Lesson_6_1 complete

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def str_to_int(string):
     result = 0
     for char in string:
         result = result * 10 + (ord(char) - ord('0'))
-    print(result)
-    print(type(result))
+    print("Decmal num in 'str_to_int: '", result)
+    print("Type of decmal num in 'str_to_int: '", type(result))
     return result
 
 
@@ -16,9 +16,18 @@ def decimal_to_binary(decimal):
     while decimal > 0:
         binary = str(decimal % 2) + binary
         decimal //= 2
-    print(binary)
-    print(type(binary))
+    print("Binary num in 'decimal_to_binary: '", binary)
+    print("Type of binaty num in 'decimal_to_binary: '", type(binary))
     return binary
 
 
-decimal_to_binary(str_to_int(string))
+def binary_to_decimal(binary):
+    decimal = 0
+    for i in range(len(binary)):
+        decimal += int(binary[i]) * 2 ** (len(binary) - i - 1)
+    print("Decmal num in 'binary_to_decimal: '", decimal)
+    print("Type of decmal num in 'binary_to_decimal: '", type(decimal))
+    return decimal
+
+
+binary_to_decimal(decimal_to_binary(str_to_int(string)))
