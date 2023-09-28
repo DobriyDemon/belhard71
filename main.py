d932699e90e5f52995785a8158dc1c947fc95653
Arseniy <aarden39@gmail.com>
2023-09-28 23:28:44 +0300

    Lesson_6_9 complete

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # Created Homework branch for the main file
+cities = \
+    {
+        'Russia': ['Moscow', 'St. Petersburg', 'Novosibirsk'],
+        'USA': ['New York', 'Los Angeles', 'Chicago'],
+        'Germany': ['Berlin', 'Hamburg', 'Munich'],
+        'France': ['Paris', 'Lyon', 'Marseille'],
+        'UK': ['London', 'Manchester', 'Bristol']
+    }
+countries = list(cities.keys())
+user_input = input("Enter a city: ")
+
+for key, value in cities.items():
+    if user_input in value:
+        print(f"{user_input.title()} in the {key}")
