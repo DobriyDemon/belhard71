dac775aa0af3b98ad4a4dc03f9a5c3845c09d63a
Arseniy <aarden39@gmail.com>
2023-09-28 23:19:05 +0300

    Lesson_6_8 complete

--- main.py
+++ main.py
@@ -3,19 +3,18 @@
 
 dict_users = \
     {
-        1: {'name': 'Sergay', 'surname': 'Иванов', 'phone': '89154268789', 'email': 'ivan@gmail.com'},
-        2: {'name': 'Gregory', 'surname': 'Иванов', 'phone': '88753033519', 'email': 'greg@gmail.com'},
-        3: {'name': 'Vasa', 'surname': 'Иванов', 'phone': '89123456789', 'email': ''},
-        4: {'name': 'Nikita', 'surname': 'Иванов', 'phone': '8912236436789'},
-        5: {'name': 'Иван', 'surname': 'Иванов', 'phone': '123-456-7890', 'email': 'ivan@example.com'},
-        6: {'name': 'Петр', 'surname': 'Петров', 'phone': '098-765-4321'},
-        7: {'name': 'Сидор', 'surname': 'Сидоров', 'phone': '555-555-5555', 'email': ''}
+        1: {'name': 'Sergey', 'surname': 'Иванов', 'phone': '89154268789'},
+        2: {'name': 'Gregory', 'surname': 'Nordef', 'phone': '88753033519', 'email': 'greg@gmail.com'},
+        3: {'name': 'Vasa', 'surname': 'Gor', 'phone': '89123456789', 'email': ''},
+        4: {'name': 'Nikita', 'surname': 'Hire', 'phone': '8912236436789'},
+        5: {'name': 'Иван', 'surname': 'Irden', 'phone': '123-456-7890'},
+        6: {'name': 'Петр', 'surname': 'Петров', 'phone': '098-765-4321', 'email': 'ivan@example.com'},
+        7: {'name': 'Сидор', 'surname': 'Сидоров', 'phone': '555-555-5555', 'email': ''},
+        8: {'name': 'Grey', 'surname': 'Forcing', 'phone': '45983-555-555', 'email': ''},
+        9: {'name': 'Fred', 'surname': 'Harley', 'phone': '555-092548', 'email': 'Frad@gamil.com'},
+
     }
 
-for i in dict_users:
-    if dict_users[i]['email'] == '' or dict_users[i]['email'] is None or dict_users[i]['email'] not in dict_users[i]:
-        print(dict_users[i]['name'])
-        print(dict_users[i]['phone'])
-    else:
-        print(dict_users[i]['name'])
-        print(dict_users[i]['email'])
+for id, user_data in dict_users.items():
+    if 'email' not in user_data or user_data['email'] == '':
+        print("Name: ", user_data['name'])
