3a3ef2c23c4738b1ac08685459ffe41b54bd4782
Arseniy <aarden39@gmail.com>
2023-10-01 18:27:32 +0300

    Lesson_6_5 complete

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 # Created Homework branch for the main file
 # Task 5
-list_of_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+count_of_nums_in_list = input("Enter a count of numbers in list: ")
+list_of_nums = []
+for i in range(int(count_of_nums_in_list) + 1):
+    list_of_nums.append(i)
+list_of_nums.remove(0)
 print(list_of_nums)
 
 
 def reverse(list_of_nums):
-    list_of_nums.sort(reverse = True)
+    len_list_of_nums = len(list_of_nums)
+    for i in range(len_list_of_nums - 1, -1, -1):
+        list_of_nums.append(list_of_nums[i])
+    count = list_of_nums[0]
+    while len(list_of_nums) != len_list_of_nums:
+        list_of_nums.remove(count)
+        count += 1
     print(list_of_nums)
     return list_of_nums
 
