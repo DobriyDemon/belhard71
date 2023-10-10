baad1bab5995bf1e63385a13ce9798c583895ae8
Arseniy <aarden39@gmail.com>
2023-10-10 20:18:27 +0300

    Lesson_9 in process

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 # Created Homework branch for the main file
+# Lesson_9
+# Start to using files
+# Binary and text files
+
+# file = open("file.txt", mode="r", encoding="utf-8")
+# file = open('file.txt' - name of file, mode='r' - mode of opening file, encoding='utf-8' - what coding of opening
+# file)
+
+# file_line = file.readline()  # One line in file, on each call
+# file_lines = file.readlines()  # All file in list of strings
+# file.seek(0)  # Go to the beginning of the file, 0 - position of the cursor
+# file.flush()  # Enter text in file without closing it
+# print(file.closed)
+# file.close()
+# print(file.closed)
+
+
+# JSON type of file
+# Sereliasing - process of converting string to bytes
+# Desereliasing - process of converting bytes to string
+# loads and dumbs - with string
+# load and dump - with bytes
+# csv_file has delimeter - splitter between strings
