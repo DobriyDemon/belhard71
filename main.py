11be0ff98a3a137007427a7292d0ac610f1aad30
Arseniy <aarden39@gmail.com>
2023-10-17 10:25:42 +0300

    Lesson_11

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 # Created Homework branch for the main file
 #HomeWork Lesson_11
+from sqlalchemy import Column, INT, VARCHAR, BOOLEAN, CheckConstraint, ForeignKey, create_engine
+from sqlalchemy.orm import DeclarativeBase, Relationship
+
+
+class Base(DeclarativeBase):
+    ...
+
